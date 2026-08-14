0980a9adc94ae336ebd689fc77fde61b5126e80e
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2026-08-14 13:37:00 +0200

    update routes

--- config/routes/auth.js
+++ config/routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const AuthController = require("../../src/controllers/AuthController");
 
-router.post("/login", AuthController.login);
-router.post("/register", AuthController.register);
+router.post("/auth/login", AuthController.login);
+router.post("/users", AuthController.register);
 
 module.exports = router;
